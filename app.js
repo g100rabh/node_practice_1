a81a92628a76bf8794be04a09df2b4268f3a848a
g100rabh <gsaurabh909@gmai.com>
2024-02-16 00:01:42 +0530

    created welcome page

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req,res, )=>{
-    const name = "saurabh";
-    console.log(name);
-    process.exit();
+    // const name = "saurabh";
+    // console.log(name);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Welcome to my Node Js project</h1> </body>')
+    res.write('</html>')
+    res.end()
+
 });
 
 server.listen(4000);
