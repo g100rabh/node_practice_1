b3edfe5bb20648ad098bf9dcddeb25b5bd2fca2a
g100rabh <gsaurabh909@gmai.com>
2024-02-16 00:07:02 +0530

    created conditional response

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const server = http.createServer((req,res, )=>{
     // const name = "saurabh";
     // console.log(name);
     // process.exit();
+
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>My first page</title></head>')
-    res.write('<body><h1>Welcome to my Node Js project</h1> </body>')
+    req.url==="/home" && res.write('<body><h1>Welcome Home</h1> </body>')
+    req.url==="/about" && res.write('<body><h1>Welcome to About Us page</h1> </body>')
+    req.url==="/node" && res.write('<body><h1>Welcome Home</h1> </body>')
+
+
     res.write('</html>')
     res.end()
 
