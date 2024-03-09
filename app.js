45db698c2e4f25379213b9e08d1073ef579ac811
g100rabh <gsaurabh909@gmai.com>
2024-03-09 22:55:47 +0530

    ExpressJs

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require("http");
-const route = require("./routes")
+const express = require("express");
 
-const server = http.createServer(route.handlers);
+const app = express();
 
-console.log(route.someText)
+app.use((req,res,next)=>{
+    console.log("middleware-1");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("middleware-2")
+    // res.send("<h1>Good Express</h1>")
+    res.send({sa: 1})
+})
+
+const server = http.createServer(app);
 
 server.listen(4000);
 
