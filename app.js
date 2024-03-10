f7ecd2e8c0b9eb7a2d53dde7342c1fe1ff0e1da0
g100rabh <gsaurabh909@gmai.com>
2024-03-10 15:09:01 +0530

    body-parser

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("middleware-1");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title' /><input type='number' name='size' /><button type='submit'>Add product</button></form>")
+    // res.send({sa: 1})
 })
 
-app.use((req,res,next)=>{
-    console.log("middleware-2")
-    // res.send("<h1>Good Express</h1>")
-    res.send({sa: 1})
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
 })
 
-const server = http.createServer(app);
 
-server.listen(4000);
+app.use('/',(req,res,next)=>{
+    // console.log("middleware-2")
+    res.send("<h1>Good Express</h1>")
+    // res.send({sa: 1})
+})
+
+app.listen(4000);
 
